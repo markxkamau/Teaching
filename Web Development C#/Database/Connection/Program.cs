dd9ab87835ca1a32652fe472236b092e44705211
markxkamau <markxkamau@gmail.com>
2023-03-17 18:54:51 +0300

    Database connectivity

---
+++ Web Development C#/Database/Connection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+
+class Program
+{
+    SqlConnection connection = new SqlConnection("Host=localhost; Database=Banking_Sys; Username=root; Password=password;");
+    // Find in file.txt created on ur end
+    public void getConnection()
+    {
+        if (connection.ConnectionString != null)
+        {
+            Console.WriteLine("Connection to db successfull");
+
+        }
+        else
+        {
+            Console.WriteLine("Connection to db unsuccessfull");
+
+        }
+        // SqlCommand command = new SqlCommand("SELECT * FROM myTable", connection);
+        // SqlDataReader reader = command.ExecuteReader();
+        // while (reader.Read())
+        // {
+        //     Console.WriteLine(reader.GetString(0));
+        // }
+        // reader.Close();
+        // connection.Close();
+    }
+}
